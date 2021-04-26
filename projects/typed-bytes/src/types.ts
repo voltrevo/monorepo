76e1e657fc25ff59fae15c57533de4dc2f0e7e11
Andrew Morris <voltrevo@gmail.com>
2021-04-26 13:23:26 +1000

    Split into files

---
+++ projects/typed-bytes/src/types.ts
@@ -0,0 +1,53 @@
+type Stream = {
+  data: DataView;
+  offset: number;
+};
+
+type Encoder<T> = {
+  encode(stream: Stream, value: T): void;
+};
+
+type Decoder<T> = {
+  decode(stream: Stream): T;
+};
+
+type Tester<T> = {
+  test(value: unknown): value is T;
+};
+
+export type Bicoder<T> = (
+  & Encoder<T>
+  & Decoder<T>
+  & Tester<T>
+  & { _typeGuard: (value: T) => T }
+);
+
+// deno-lint-ignore no-explicit-any
+type ExplicitAny = any;
+
+export type AnyBicoder = Bicoder<ExplicitAny>;
+
+export type StringMap<T> = { [key in string]?: T };
+
+type BicoderTargetsImpl<T> = (
+  T extends [Bicoder<infer First>, ...infer Rest]
+    ? [First, ...BicoderTargetsImpl<Rest>]
+    : []
+);
+
+export type BicoderTargets<T extends AnyBicoder[]> = BicoderTargetsImpl<T>;
+
+export type TypeOf<B extends AnyBicoder> = B extends Bicoder<infer T> ? T
+  : never;
+
+export type UnionOf<T extends unknown[]> = (
+  T extends [infer First, ...infer Rest] ? First | UnionOf<Rest>
+    : never
+);
+
+export type Primitive =
+  | undefined
+  | null
+  | boolean
+  | number
+  | string;
