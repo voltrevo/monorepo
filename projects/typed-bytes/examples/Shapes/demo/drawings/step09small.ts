fce2044ac55aab71c6aa98592d1582bee4c9a3f7
Andrew Morris <voltrevo@gmail.com>
2021-04-29 01:21:16 +1000

    Finish converting step 9 drawing, add small variation

--- projects/typed-bytes/examples/Shapes/demo/drawings/step09small.ts
+++ projects/typed-bytes/examples/Shapes/demo/drawings/step09small.ts
@@ -9,7 +9,137 @@ export default ensureType<shapes.Drawing>()({
     background: null,
   },
   registry: {
-    door: [
+    w: [
+      {
+        type: "square",
+        position: { x: 0, y: 0 },
+        sideLength: 30,
+        rotation: 0,
+        outline: {
+          thickness: 1,
+          color: colors.black,
+        },
+        fill: colors.white,
+      },
+      {
+        type: "square",
+        position: { x: -7, y: -7 },
+        sideLength: 15,
+        rotation: 0,
+        outline: {
+          thickness: 1,
+          color: colors.black,
+        },
+        fill: colors.white,
+      },
+      {
+        type: "square",
+        position: { x: 7, y: 7 },
+        sideLength: 15,
+        rotation: 0,
+        outline: {
+          thickness: 1,
+          color: colors.black,
+        },
+        fill: colors.white,
+      },
+    ],
+  },
+  shape: {
+    type: "transformer",
+    origin: { x: 640, y: 360 },
+    rotate: null,
+    scale: null,
+    shape: [ // Only have squares so I'm making a rectangle using three
+      {
+        type: "square",
+        position: { x: 25, y: -128 },
+        sideLength: 16,
+        rotation: 0,
+        outline: {
+          thickness: 1,
+          color: colors.black,
+        },
+        fill: { red: 100, green: 50, blue: 0, alpha: 255 },
+      },
+      {
+        type: "square",
+        position: { x: 25, y: -139 },
+        sideLength: 16,
+        rotation: 0,
+        outline: {
+          thickness: 1,
+          color: colors.black,
+        },
+        fill: { red: 100, green: 50, blue: 0, alpha: 255 },
+      },
+      {
+        type: "square",
+        position: { x: 25, y: -149 },
+        sideLength: 16,
+        rotation: 0,
+        outline: {
+          thickness: 1,
+          color: colors.black,
+        },
+        fill: { red: 100, green: 50, blue: 0, alpha: 255 },
+      },
+
+      // I needed the outline so I'm using a third square to cover up the line
+      // in between the two squares above
+      {
+        type: "square",
+        position: { x: 25, y: -136 },
+        sideLength: 14,
+        rotation: 0,
+        outline: null,
+        fill: { red: 100, green: 50, blue: 0, alpha: 255 },
+      },
+      {
+        type: "circle",
+        position: { x: 30, y: -170 },
+        radius: 8,
+        outline: null,
+        fill: { red: 255, green: 255, blue: 255, alpha: 200 },
+      },
+      {
+        type: "circle",
+        position: { x: 38, y: -181 },
+        radius: 12,
+        outline: null,
+        fill: { red: 255, green: 255, blue: 255, alpha: 150 },
+      },
+      {
+        type: "circle",
+        position: { x: 52, y: -188 },
+        radius: 16,
+        outline: null,
+        fill: { red: 255, green: 255, blue: 255, alpha: 100 },
+      },
+      // main
+      {
+        type: "square",
+        position: { x: 0, y: -50 },
+        sideLength: 100,
+        rotation: 0,
+        outline: {
+          thickness: 3,
+          color: colors.black,
+        },
+        fill: { red: 255, green: 255, blue: 0, alpha: 255 },
+      },
+      // roof
+      {
+        type: "triangle",
+        position: { x: 0, y: -125 },
+        sideLength: 100,
+        rotation: 0,
+        outline: {
+          thickness: 3,
+          color: colors.black,
+        },
+        fill: { red: 255, green: 180, blue: 50, alpha: 255 },
+      },
       // Only have squares so I'm making a rectangle using two
       {
         type: "square",
@@ -55,173 +185,21 @@ export default ensureType<shapes.Drawing>()({
         outline: null,
         fill: colors.black,
       },
-    ],
-    houseFrame: [
-      // main
-      {
-        type: "square",
-        position: { x: 0, y: -50 },
-        sideLength: 100,
-        rotation: 0,
-        outline: {
-          thickness: 3,
-          color: colors.black,
-        },
-        fill: { red: 255, green: 255, blue: 0, alpha: 255 },
-      },
-      // roof
-      {
-        type: "triangle",
-        position: { x: 0, y: -125 },
-        sideLength: 100,
-        rotation: 0,
-        outline: {
-          thickness: 3,
-          color: colors.black,
-        },
-        fill: { red: 255, green: 180, blue: 50, alpha: 255 },
-      },
-    ],
-    window: [
-      {
-        type: "square",
-        position: { x: 0, y: 0 },
-        sideLength: 30,
-        rotation: 0,
-        outline: {
-          thickness: 1,
-          color: colors.black,
-        },
-        fill: colors.white,
-      },
-      {
-        type: "square",
-        position: { x: -7, y: -7 },
-        sideLength: 15,
-        rotation: 0,
-        outline: {
-          thickness: 1,
-          color: colors.black,
-        },
-        fill: colors.white,
-      },
-      {
-        type: "square",
-        position: { x: 7, y: 7 },
-        sideLength: 15,
-        rotation: 0,
-        outline: {
-          thickness: 1,
-          color: colors.black,
-        },
-        fill: colors.white,
-      },
-    ],
-    windowPair: [
-      {
-        type: "transformer",
-        origin: { x: -23, y: 0 },
-        rotate: null,
-        scale: null,
-        shape: "window",
-      },
-      {
-        type: "transformer",
-        origin: { x: 23, y: 0 },
-        rotate: null,
-        scale: null,
-        shape: "window",
-      },
-    ],
-    chimney: [
-      // Only have squares so I'm making a rectangle using three
-      {
-        type: "square",
-        position: { x: 25, y: -128 },
-        sideLength: 16,
-        rotation: 0,
-        outline: {
-          thickness: 1,
-          color: colors.black,
-        },
-        fill: { red: 100, green: 50, blue: 0, alpha: 255 },
-      },
-      {
-        type: "square",
-        position: { x: 25, y: -139 },
-        sideLength: 16,
-        rotation: 0,
-        outline: {
-          thickness: 1,
-          color: colors.black,
-        },
-        fill: { red: 100, green: 50, blue: 0, alpha: 255 },
-      },
-      {
-        type: "square",
-        position: { x: 25, y: -149 },
-        sideLength: 16,
-        rotation: 0,
-        outline: {
-          thickness: 1,
-          color: colors.black,
-        },
-        fill: { red: 100, green: 50, blue: 0, alpha: 255 },
-      },
 
-      // I needed the outline so I'm using a third square to cover up the line
-      // in between the two squares above
-      {
-        type: "square",
-        position: { x: 25, y: -136 },
-        sideLength: 14,
-        rotation: 0,
-        outline: null,
-        fill: { red: 100, green: 50, blue: 0, alpha: 255 },
-      },
-    ],
-    smoke: [
-      {
-        type: "circle",
-        position: { x: 30, y: -170 },
-        radius: 8,
-        outline: null,
-        fill: { red: 255, green: 255, blue: 255, alpha: 200 },
-      },
-      {
-        type: "circle",
-        position: { x: 38, y: -181 },
-        radius: 12,
-        outline: null,
-        fill: { red: 255, green: 255, blue: 255, alpha: 150 },
-      },
-      {
-        type: "circle",
-        position: { x: 52, y: -188 },
-        radius: 16,
-        outline: null,
-        fill: { red: 255, green: 255, blue: 255, alpha: 100 },
-      },
-    ],
-    house: [
-      "chimney",
-      "houseFrame",
-      "door",
       {
         type: "transformer",
-        origin: { x: 0, y: -73 },
+        origin: { x: -23, y: -73 },
         rotate: null,
         scale: null,
-        shape: "windowPair",
+        shape: "w",
       },
-      "smoke",
+      {
+        type: "transformer",
+        origin: { x: 23, y: -73 },
+        rotate: null,
+        scale: null,
+        shape: "w",
+      },
     ],
-  },
-  shape: {
-    type: "transformer",
-    origin: { x: 640, y: 360 },
-    rotate: null,
-    scale: null,
-    shape: "house",
   },
 });
--- projects/typed-bytes/examples/Shapes/demo/writePng.ts
+++ projects/typed-bytes/examples/Shapes/demo/writePng.ts
@@ -1,4 +1,9 @@
+import * as tb from "../../../mod.ts";
+
 import render from "./render.ts";
-import drawing from "./drawings/step08a.ts";
+import drawing from "./drawings/step09small.ts";
+import * as shapes from "./shapes.ts";
 
 await Deno.writeFile("./drawing.png", render(drawing));
+
+console.log(tb.encodeBuffer(shapes.Drawing, drawing).byteLength, "bytes");
