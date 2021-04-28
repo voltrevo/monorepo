20d5129dbe26e0bf76920f5c099c9f3ac4f73503
Andrew Morris <voltrevo@gmail.com>
2021-04-28 17:11:11 +1000

    Update step 8

--- projects/typed-bytes/examples/Shapes/tutorial/step08/README.md
+++ projects/typed-bytes/examples/Shapes/tutorial/step08/README.md
@@ -4,9 +4,89 @@ Look, I drew a house:
 
 ![Drawing 1](./drawing_1.png)
 
-314 bytes
+[Code](./step08_1.ts)
 
-(If you're an artist, I'm really very sorry. Please see [CONTRIBUTING.md](../../../../CONTRIBUTING.md)
-😄).
+(If you're an artist, I'm really very sorry. Please see
+[CONTRIBUTING.md](../../../../CONTRIBUTING.md) 😄.)
 
-Isn't it great?
+Isn't it great? We should make lots of houses just like this.
+
+It only takes up 314 bytes, but do we really want to pay 314 bytes for each
+house? Nah. Besides, grouping things together will have other benefits.
+
+We like shapes, so let's put shapes in our shapes:
+
+```ts
+const MetaShape = tb.object({
+  type: tb.exact("meta"),
+  shapes: tb.array(Shape),
+});
+```
+
+And add this to our `Shape` definition...
+
+```diff
+-const Shape = tb.union(Circle, Triangle, Square);
++const Shape = tb.union(Circle, Triangle, Square, MetaShape);
+```
+
+... uh oh, `MetaShape` needed to be defined after `Shape` but now `Shape` needs
+to refer to `MetaShape`.
+
+Introducing `tb.defer`. This utility allows you to create *recursive* bicoders:
+
+```ts
+// It's ok to have this before defining Shape
+const ShapeReference = tb.defer(() => Shape);
+```
+
+Now we can just use `ShapeReference` as we would `Shape`:
+
+```diff
+ const MetaShape = tb.object({
+   type: tb.exact("meta"),
+-  shapes: tb.array(Shape),
++  shapes: tb.array(ShapeReference),
+ });
+```
+
+and then we define `Shape` last as before:
+
+```ts
+const Shape = tb.union(Circle, Triangle, Square, MetaShape);
+```
+
+One more problem though. When you use `tb.defer`, you need to specify the type
+explicitly, otherwise you'll get a type error like this:
+
+```
+'ShapeReference' implicitly has type 'any' because it does not have a type annotation and is referenced directly or indirectly in its own initializer.
+```
+
+It's kinda fair enough. In principle it may be possible for TypeScript to infer
+the recursive type, but I imagine that could lead to some very confusing edge
+cases (for users of TypeScript I mean, though I assume it would be tricky for
+the TypeScript team to implement too).
+
+Anyway, we'll need some duplication between our structure definitions and some
+explicit typing. Only in this recursive interaction between `MetaShape` and
+`Shape` though, we can still use the inferred types for `Circle`, `Square`, and
+`Triangle`:
+
+```ts
+type MetaShape = {
+  type: "meta";
+  shapes: Shape[];
+};
+
+type Shape =
+  | Circle
+  | Triangle
+  | Square
+  | MetaShape;
+```
+
+```diff
+-const ShapeReference = tb.defer(() => Shape);
++const ShapeReference: tb.Bicoder<Shape> = tb.defer(() => Shape);
+```
---
+++ projects/typed-bytes/examples/Shapes/tutorial/step08/step08_2.ts
@@ -0,0 +1,314 @@
+import * as tb from "../../../../mod.ts";
+
+const Color = tb.object({
+  red: tb.byte,
+  green: tb.byte,
+  blue: tb.byte,
+  alpha: tb.byte,
+});
+
+const black = { red: 0, green: 0, blue: 0, alpha: 255 };
+const red = { red: 255, green: 0, blue: 0, alpha: 255 };
+const white = { red: 255, green: 255, blue: 255, alpha: 255 };
+
+const Canvas = tb.object({
+  width: tb.size,
+  height: tb.size,
+  background: tb.optional(Color),
+});
+
+const Position = tb.object({
+  x: tb.isize, // Note: `isize` is like `size` but it allows negative numbers
+  y: tb.isize,
+});
+
+const outlineAndFill = {
+  outline: tb.optional(tb.object({
+    color: Color,
+    thickness: tb.size,
+  })),
+  fill: tb.optional(Color),
+};
+
+const Circle = tb.object({
+  type: tb.exact("circle"),
+  position: Position,
+  radius: tb.size,
+  ...outlineAndFill,
+});
+
+type Circle = tb.TypeOf<typeof Circle>;
+
+const Triangle = tb.object({
+  type: tb.exact("triangle"),
+  position: Position,
+  sideLength: tb.size,
+  rotation: tb.isize,
+  ...outlineAndFill,
+});
+
+type Triangle = tb.TypeOf<typeof Triangle>;
+
+const Square = tb.object({
+  type: tb.exact("square"),
+  position: Position,
+  sideLength: tb.size,
+  rotation: tb.isize,
+  ...outlineAndFill,
+});
+
+type Square = tb.TypeOf<typeof Square>;
+
+type MetaShape = {
+  type: "meta";
+  shapes: Shape[];
+};
+
+type Shape =
+  | Circle
+  | Triangle
+  | Square
+  | MetaShape;
+
+const ShapeReference: tb.Bicoder<Shape> = tb.defer(() => Shape);
+
+const MetaShape = tb.object({
+  type: tb.exact("meta"),
+  shapes: tb.array(ShapeReference),
+});
+
+const Shape = tb.union(Circle, Triangle, Square, MetaShape);
+
+const shapes: Shape[] = [
+  // chimney
+
+  // Only have squares so I'm making a rectangle using three
+  {
+    type: "square",
+    position: { x: 640 + 25, y: 360 - 128 },
+    sideLength: 16,
+    rotation: 0,
+    outline: {
+      thickness: 1,
+      color: black,
+    },
+    fill: { red: 100, green: 50, blue: 0, alpha: 255 },
+  },
+  {
+    type: "square",
+    position: { x: 640 + 25, y: 360 - 139 },
+    sideLength: 16,
+    rotation: 0,
+    outline: {
+      thickness: 1,
+      color: black,
+    },
+    fill: { red: 100, green: 50, blue: 0, alpha: 255 },
+  },
+  {
+    type: "square",
+    position: { x: 640 + 25, y: 360 - 149 },
+    sideLength: 16,
+    rotation: 0,
+    outline: {
+      thickness: 1,
+      color: black,
+    },
+    fill: { red: 100, green: 50, blue: 0, alpha: 255 },
+  },
+
+  // I needed the outline so I'm using a third square to cover up the line in
+  // between the two squares above
+  {
+    type: "square",
+    position: { x: 640 + 25, y: 360 - 136 },
+    sideLength: 14,
+    rotation: 0,
+    outline: null,
+    fill: { red: 100, green: 50, blue: 0, alpha: 255 },
+  },
+
+  // house
+  // main
+  {
+    type: "square",
+    position: { x: 640, y: 360 - 50 },
+    sideLength: 100,
+    rotation: 0,
+    outline: {
+      thickness: 3,
+      color: black,
+    },
+    fill: { red: 255, green: 255, blue: 0, alpha: 255 },
+  },
+  // roof
+  {
+    type: "triangle",
+    position: { x: 640, y: 360 - 125 },
+    sideLength: 100,
+    rotation: 0,
+    outline: {
+      thickness: 3,
+      color: black,
+    },
+    fill: { red: 255, green: 180, blue: 50, alpha: 255 },
+  },
+
+  // door
+
+  // Only have squares so I'm making a rectangle using two
+  {
+    type: "square",
+    position: { x: 640, y: 360 - 14 },
+    sideLength: 24,
+    rotation: 0,
+    outline: {
+      thickness: 1,
+      color: black,
+    },
+    fill: red,
+  },
+  {
+    type: "square",
+    position: { x: 640, y: 360 - 35 },
+    sideLength: 24,
+    rotation: 0,
+    outline: {
+      thickness: 1,
+      color: black,
+    },
+    fill: red,
+  },
+
+  // I needed the outline so I'm using a third square to cover up the line in
+  // between the two squares above
+  {
+    type: "square",
+    position: { x: 640, y: 360 - 20 },
+    sideLength: 22,
+    rotation: 0,
+    outline: null,
+    fill: red,
+  },
+
+  // Handle
+  {
+    // Because there's no anti-aliasing (yet?) an r=2 circle actually renders
+    // as a square hah
+    type: "circle",
+    position: { x: 640 - 7, y: 360 - 22 },
+    radius: 2,
+    outline: null,
+    fill: black,
+  },
+
+  // windows
+
+  // Left
+  {
+    type: "square",
+    position: { x: 640 - 23, y: 360 - 73 },
+    sideLength: 30,
+    rotation: 0,
+    outline: {
+      thickness: 1,
+      color: black,
+    },
+    fill: white,
+  },
+  {
+    type: "square",
+    position: { x: 640 - 30, y: 360 - 80 },
+    sideLength: 15,
+    rotation: 0,
+    outline: {
+      thickness: 1,
+      color: black,
+    },
+    fill: white,
+  },
+  {
+    type: "square",
+    position: { x: 640 - 16, y: 360 - 66 },
+    sideLength: 15,
+    rotation: 0,
+    outline: {
+      thickness: 1,
+      color: black,
+    },
+    fill: white,
+  },
+
+  // Right
+  {
+    type: "square",
+    position: { x: 640 + 23, y: 360 - 73 },
+    sideLength: 30,
+    rotation: 0,
+    outline: {
+      thickness: 1,
+      color: black,
+    },
+    fill: white,
+  },
+  {
+    type: "square",
+    position: { x: 640 + 16, y: 360 - 80 },
+    sideLength: 15,
+    rotation: 0,
+    outline: {
+      thickness: 1,
+      color: black,
+    },
+    fill: white,
+  },
+  {
+    type: "square",
+    position: { x: 640 + 30, y: 360 - 66 },
+    sideLength: 15,
+    rotation: 0,
+    outline: {
+      thickness: 1,
+      color: black,
+    },
+    fill: white,
+  },
+
+  // smoke
+
+  {
+    type: "circle",
+    position: { x: 640 + 30, y: 360 - 170 },
+    radius: 8,
+    outline: null,
+    fill: { red: 255, green: 255, blue: 255, alpha: 200 },
+  },
+  {
+    type: "circle",
+    position: { x: 640 + 38, y: 360 - 181 },
+    radius: 12,
+    outline: null,
+    fill: { red: 255, green: 255, blue: 255, alpha: 150 },
+  },
+  {
+    type: "circle",
+    position: { x: 640 + 52, y: 360 - 188 },
+    radius: 16,
+    outline: null,
+    fill: { red: 255, green: 255, blue: 255, alpha: 100 },
+  },
+];
+
+const Drawing = tb.object({
+  canvas: Canvas,
+  shapes: tb.array(Shape),
+});
+
+type Drawing = tb.TypeOf<typeof Drawing>;
+
+const drawing: Drawing = {
+  canvas: { width: 1280, height: 720, background: null },
+  shapes,
+};
+
+console.log(new Uint8Array(tb.encodeBuffer(Drawing, drawing)));
