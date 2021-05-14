269e833f823a12a03349ae7e4365fe2b123f272d
Andrew Morris <voltrevo@gmail.com>
2021-05-14 01:21:23 +0000

    sendBicoder -> Request

--- projects/typed-bytes/src/rpc/protocol.ts
+++ projects/typed-bytes/src/rpc/protocol.ts
@@ -13,7 +13,7 @@ type ValuesOf<Obj> = Obj[keyof Obj];
 type Protocol<Spec extends SpecBase> = {
   type: "protocol";
   spec: Spec;
-  sendBicoder: tb.Bicoder<
+  Request: tb.Bicoder<
     ValuesOf<
       {
         [MethodName in keyof Spec]: MethodMessage<Spec, MethodName>;
@@ -27,19 +27,19 @@ export default function protocol<Spec extends SpecBase>(
 ): Protocol<Spec> {
   const methodNames = Object.keys(spec);
 
-  const sendBicoder = tb.union(
+  const Request = tb.union(
     ...methodNames.map((methodName) =>
       tb.object({
         method: tb.exact(methodName),
         args: tb.tuple(...spec[methodName].args),
       })
     ),
-  ) as unknown as Protocol<Spec>["sendBicoder"];
+  ) as unknown as Protocol<Spec>["Request"];
 
   return {
     type: "protocol",
     spec,
-    sendBicoder,
+    Request,
   };
 }
 
